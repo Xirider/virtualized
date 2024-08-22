1c1d1378b47aef86fbd27cb0c9a2fcc1dbacad4f
Xirider <peter.k.albert@gmail.com>
2024-08-22 13:46:57 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, { Children, FC } from 'react'
+import './App.css'
+import { List } from './components/List';
+import { useDictionary } from './hooks/useDictionary'
+
+
+function App() {
+  const dictionary = useDictionary();
+  return (
+    <div className="app">
+      <div className="header">
+        <div>
+          <span>React Virtualized List</span>
+        </div>
+      </div>
+      <div className="content">
+        <List items={dictionary} />
+      </div>
+    </div>
+  )
+}
+
+export default App
